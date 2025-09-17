b41b105ac0598330cda86df1adac17332c6e97b8
duythinh26 <pduythinh79@gmail.com>
2025-09-17 21:04:26 +0700

    create UI for none tab left

---
+++ src/components/EmptyState.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FilePlus2 } from 'lucide-react';
+import { useTabs } from '../hooks/useTabs';
+
+const EmptyState = () => {
+
+    const { addTab } = useTabs()
+
+    return (
+        <div className="flex flex-col items-center justify-center h-full text-gray-500 mt-20">
+            <FilePlus2 size={64} strokeWidth={1} />
+            <p className="mt-4 text-lg">Không có tài liệu nào đang mở</p>
+            <button
+                onClick={addTab} 
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors"
+            >
+                Tạo tài liệu mới
+            </button>
+        </div>
+    )
+}
+
+export default EmptyState
